45b518a6517386ac07b2328c9b285739fe5e57cf
Sean Meng <seanmeng2018@gmail.com>
2024-05-27 15:05:32 -0400

    a

--- app/src/main/java/com/example/trainingarc/MainActivity.kt
+++ app/src/main/java/com/example/trainingarc/MainActivity.kt
@@ -16,32 +16,7 @@ import com.example.trainingarc.ui.theme.TrainingArcTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContent {
-            TrainingArcTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
-        }
+        setContentView(R.layout.layout)
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TrainingArcTheme {
-        Greeting("Android")
-    }
-}
